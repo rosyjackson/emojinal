a2e4205f64f6109c2e8e17fe9866e9cf5146fd82
Apple <apple@Apples-MacBook-Pro-106.local>
2019-06-07 15:21:35 -0500

    commiting changes to first alert

--- emojinal/ViewController.swift
+++ emojinal/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func showMessage(sender: UIButton) {
+//        click on emoji -> shows message based on emoji
+        let alertController = UIAlertController(title: "Emoji", message: "*sending virtual hug*", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+
     }
 
 
