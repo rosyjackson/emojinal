eb2c648f2a279ccd9fe97d82a406a2ff025bfccd
Apple <apple@Apples-MacBook-Pro-106.local>
2019-06-07 16:29:53 -0500

    iteration 3

--- emojinal/ViewController.swift
+++ emojinal/ViewController.swift
@@ -9,19 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
+    let emojis = ["😞": "disappointed", "😰": "worried"]
+    
+    let customMessages = [
+        "disappointed": ["cheer up buttercup", "take a deep breathe", "do you need to talk?"],
+                          "worried": ["cheer up buttercup2", "take a deep breathe2", "do you need to talk?2"]]
+    let number = Int.random(in: 0 ..< 3)
 
+
+    
     @IBAction func showMessage(sender: UIButton) {
         let selectedEmotion = sender.titleLabel?.text
 //        click on emoji -> shows message based on emoji
-        let alertController = UIAlertController(title: "Emoji", message: "\(emojis.values)", preferredStyle: UIAlertController.Style.alert)
+
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[number]
+
+        
+        let alertController = UIAlertController(title: "Emoji", message: "\(emojiMessage)", preferredStyle: UIAlertController.Style.alert)
         
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
 
     }
     
-    let emojis = ["😞": "disappointed", "😰": "worried"]
-
+   
 
 
 }
