c761ae497a7098d10f34101d4a3ad2e5de34a30c
Apple <apple@Apples-MacBook-Pro-106.local>
2019-06-07 15:33:53 -0500

    Itertaion 2 commit

--- emojinal/ViewController.swift
+++ emojinal/ViewController.swift
@@ -11,13 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func showMessage(sender: UIButton) {
+        let selectedEmotion = sender.titleLabel?.text
 //        click on emoji -> shows message based on emoji
-        let alertController = UIAlertController(title: "Emoji", message: "*sending virtual hug*", preferredStyle: UIAlertController.Style.alert)
+        let alertController = UIAlertController(title: "Emoji", message: "\(emojis.values)", preferredStyle: UIAlertController.Style.alert)
         
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
 
     }
+    
+    let emojis = ["😞": "disappointed", "😰": "worried"]
+
 
 
 }
